40e365a500394dcfcc8042e8d4f529dd5f5784d9
mo <=>
2022-10-03 17:39:39 +0200

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+updateCountdown = () => {
+    // releas edate is May 12, 2023
+    const releaseDateUTC = 1683896400000;
+    const nowDateUTC = new Date().getTime();
+    const daysLeftUntilRelease = 
+        ((releaseDateUTC - nowDateUTC) / 1000 / 60 / 60 / 24)
+            .toString()
+            .split(".")[0];
+
+    document.getElementById("daysCount").innerHTML = daysLeftUntilRelease + " DAYS";
+}
+
+init = () => {
+    updateCountdown()
+    setInterval(updateCountdown, (1000 * 60))
+} 
+
+window.onload = init;
