511ed13c9b69f9c07e2fd75fe6d76ec7c0a64f30
mo <=>
2022-10-03 17:57:09 +0200

    added cool stuff

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ updateCountdown = () => {
     // releas edate is May 12, 2023
     const releaseDateUTC = 1683896400000;
     const nowDateUTC = new Date().getTime();
-    const daysLeftUntilRelease = 
+    const daysLeftUntilRelease =
         ((releaseDateUTC - nowDateUTC) / 1000 / 60 / 60 / 24)
             .toString()
             .split(".")[0];
 
+    document.getElementsByTagName("title")[0].innerHTML = daysLeftUntilRelease + " days left";
     document.getElementById("daysCount").innerHTML = daysLeftUntilRelease + " DAYS";
 }
 
 init = () => {
     updateCountdown()
     setInterval(updateCountdown, (1000 * 60))
-} 
+}
 
 window.onload = init;
